6653b9d8c734e5eb6b6fd3e2b660c92aeb6dedd5
SalaamAzeezat <38332342+SalaamAzeezat@users.noreply.github.com>
2020-03-18 17:51:49 +0100

    new
    new

---
+++ js/a.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('#navbar_list');
+const newElement = document.createElement('li');
+
+for (let i = 1; i <= 3; i++) {
+    const anchorElement = document.createElement('a');
+      newElement.appendChild(anchorElement);
+}
+document.nav.appendChild(newElement);
+let navBar = document.querySelector("#navbar_list");
+let newElement = document.createElement("li");
+for (let i = 1; i <= 3; i++) {
+let anchorElement = document.createElement("a");
+let anchorContent = document.createTextNode("Section 1");
+anchorElement.appendChild(anchorContent);
+newElement.appendChild(anchorElement);
+}
+ul.appendChild(newElement);
--- js/app.js
+++ js/app.js
@@ -33,8 +33,15 @@
 */
 
 // build the nav
-
-
+const newElement =document.createElement('li'); 
+for(let I = 1; I <=3; I++) { 
+         const anchorElement = document.createElement('a');
+         anchorElement.innerHTML = 'section_1'
+        newElement.appendChild(anchorElement); 
+}
+const navBar = document.getElementById('#navbar_list');
+navBar.appendChild(newElement);
+document.body.append(navBar);
 // Add class 'active' to section when near top of viewport
 
 
